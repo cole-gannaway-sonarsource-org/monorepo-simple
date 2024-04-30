8bf475d31662f0036756a8410c50dc0e3b02b2f4
Cole Gannaway <156719330+cole-gannaway-sonarsource@users.noreply.github.com>
2024-04-30 10:38:17 -0500

    added security issue random too predictable

--- backend/src/main/java/com/mycompany/app/App.java
+++ backend/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import java.security.SecureRandom;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(123456L); // Noncompliant
+        int v = sr.nextInt(32);
+        System.out.println(v);
     }
 }
