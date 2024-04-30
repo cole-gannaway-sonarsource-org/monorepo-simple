d454a9d539d29ad1221d87951405aa7f9c02a662
Cole Gannaway <156719330+cole-gannaway-sonarsource@users.noreply.github.com>
2024-04-30 10:16:28 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,11 @@ function App() {
       </header>
     </div>
   );
+  function doBadThings(input){
+    const numbers = [10, 2, 30, 1, 5];
+    numbers.sort(); // Noncompliant: lexicographic sort
+    console.log(numbers); // Output: [1, 10, 2, 30, 5]
+  }
 }
 
 export default App;
